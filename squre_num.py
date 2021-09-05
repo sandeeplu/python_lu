40e583a41bfb19af87a57d5a267021e818d78013
Sandeep Kumar <sandeepk.phy@gmail.com>
2021-09-05 12:52:27 +0200

    more numer

--- squre_num.py
+++ squre_num.py
@@ -4,6 +4,6 @@ def get_square_numbers(numbers):
          square_numbers.append(n*n)
     return square_numbers
 
-numbers = [2,5,8,9]
+numbers = [2,5,8,9,10,11]
 a = get_square_numbers(numbers)
 print(a)
